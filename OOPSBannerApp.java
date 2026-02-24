97c1627e2f3be55ef9339eb5a627a88838492d6b
Bhuvan <bhuvanchandranalabothu@gmail.com>
2026-02-24 10:14:34 +0530

    Printing the OOPS in the console

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,19 +1,34 @@
 /**
  * OOPSBannerApp
- * 
- * This program prints the text "OOPS" to the console.
- * It demonstrates the basic structure of a Java application.
- * 
+ *
+ * UC2: Render OOPS as Banner using Print Statements
+ *
  * @author Bhuvan
- * @version 1.0
+ * @version 2.0
  */
 public class OOPSBannerApp {
 
-    /**
-     * Main method - Entry point of the Java application
-     * @param args Command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        
+        System.out.println("  *****   " + "  *****   " + "  ******  " + "  ******  ");
+
+        
+        System.out.println(" *     *  " + " *     *  " + " *     *  " + " *        ");
+
+    
+        System.out.println(" *     *  " + " *     *  " + " *     *  " + " *        ");
+
+        
+        System.out.println(" *     *  " + " *     *  " + "  ******  " + "  *****   ");
+
+        
+        System.out.println(" *     *  " + " *     *  " + " *        " + "       *  ");
+
+        
+        System.out.println(" *     *  " + " *     *  " + " *        " + "       *  ");
+
+        
+        System.out.println("  *****   " + "  *****   " + " *        " + "  *****   ");
     }
 }
